e0e8c3164dc9ba6a60fa46674467d6680e4e23de
ahsan <makabir98@yahoo.com>
2024-09-16 19:50:05 +0600

    first temp made for testing

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// import styles from './Footer.module.css';
+
+export default function Footer () {
+    return (
+        <div 
+            // className={styles.appFooter}
+        >
+            {/* <p className={styles.p}>Copyright &copy; Mohammed Ahsanul Kabir</p> */}
+            <p >Copyright &copy; Mohammed Ahsanul Kabir</p>
+        </div>
+    )
+}
---
+++ src/components/Layout.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+// import styles from './Layout.module.css'
+
+const Layout = () => {
+    return (
+        <div 
+            // className={styles.layout}
+        >
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    );
+};
+
+export default Layout;
